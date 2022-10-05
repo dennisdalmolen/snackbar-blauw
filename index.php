7d783e14f18d1a5a159c48bc1db390c52a35a11c
dennisdalmolen <dennisssdalmolennn@gmail.com>
2022-10-05 11:22:49 +0200

    Index Changes

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="responsive.css">
     <title>Snackbar!</title>
 </head>
 <header>
@@ -48,55 +49,205 @@
 
         <div class="product item-1">
             <div class="product-in"><img src="images/product1.png" alt="">
-                <h5>broodj</h5>
+                <div class="product-in-text">
+                    <h5>Broodj Menu</h5>
+                    <h5>€5,-</h5>
+                </div>
+                <div class="product-in-lower">
+                    <input type="number" id="quantity" name="quantity" min="1" max="10" placeholder="Aantal">
+                    <div class="btn-toevoegen">
+                        <a href="#">
+                            <h4>Toevoegen!</h4>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
 
         <div class="product item-2">
             <div class="product-in"><img src="images/product2.png" alt="">
-                <h5>broodj</h5>
+                <div class="product-in-text">
+                    <h5>Dubbele Broodj</h5>
+                    <h5>€3,-</h5>
+                </div>
+                <div class="product-in-lower">
+                    <input type="number" id="quantity" name="quantity" min="1" max="10" placeholder="Aantal">
+                    <div class="btn-toevoegen">
+                        <a href="#">
+                            <h4>Toevoegen!</h4>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
 
         <div class="product item-3">
-            <div class="product-in"><img src="images/product3.png" alt="">
-                <h5>broodj</h5>
+            <div class="product-in"><img src="images/product6.png" alt="">
+                <div class="product-in-text">
+                    <h5>Dubbele Broodj Deluxe</h5>
+                    <h5>€4,-</h5>
+                </div>
+                <div class="product-in-lower">
+                    <input type="number" id="quantity" name="quantity" min="1" max="10" placeholder="Aantal">
+                    <div class="btn-toevoegen">
+                        <a href="#">
+                            <h4>Toevoegen!</h4>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
 
         <div class="product item-4">
-            <div class="product-in"><img src="images/product4.png" alt="">
-                <h5>broodj</h5>
+            <div class="product-in"><img src="images/product5.png" alt="">
+                <div class="product-in-text">
+                    <h5>Saai Broodj</h5>
+                    <h5>€1,-</h5>
+                </div>
+                <div class="product-in-lower">
+                    <input type="number" id="quantity" name="quantity" min="1" max="10" placeholder="Aantal">
+                    <div class="btn-toevoegen">
+                        <a href="#">
+                            <h4>Toevoegen!</h4>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
 
         <div class="product item-5">
-            <div class="product-in"><img src="images/product5.png" alt="">
-                <h5>broodj</h5>
+            <div class="product-in"><img src="images/product4.png" alt="">
+                <div class="product-in-text">
+                    <h5>Kaas Broodj</h5>
+                    <h5>€1,50</h5>
+                </div>
+                <div class="product-in-lower">
+                    <input type="number" id="quantity" name="quantity" min="1" max="10" placeholder="Aantal">
+                    <div class="btn-toevoegen">
+                        <a href="#">
+                            <h4>Toevoegen!</h4>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
 
         <div class="product item-6">
-            <div class="product-in"><img src="images/product6.png" alt="">
-                <h5>broodj</h5>
+            <div class="product-in"><img src="images/product3.png" alt="">
+                <div class="product-in-text">
+                    <h5>Extra Kaas Broodj</h5>
+                    <h5>€2,-</h5>
+                </div>
+                <div class="product-in-lower">
+                    <input type="number" id="quantity" name="quantity" min="1" max="10" placeholder="Aantal">
+                    <div class="btn-toevoegen">
+                        <a href="#">
+                            <h4>Toevoegen!</h4>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
 
         <div class="product item-7">
-            <div class="product-in"><img src="images/product7.png" alt="">
-                <h5>broodj</h5>
+            <div class="product-in"><img src="images/product8.png" alt="">
+                <div class="product-in-text">
+                    <h5>Vis Broodj</h5>
+                    <h5>€2,-</h5>
+                </div>
+                <div class="product-in-lower">
+                    <input type="number" id="quantity" name="quantity" min="1" max="10" placeholder="Aantal">
+                    <div class="btn-toevoegen">
+                        <a href="#">
+                            <h4>Toevoegen!</h4>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
 
         <div class="product item-8">
-            <div class="product-in"><img src="images/product8.png" alt="">
-                <h5>broodj</h5>
+            <div class="product-in"><img src="images/product12.png" alt="">
+                <div class="product-in-text">
+                    <h5>Ontbijt Broodj</h5>
+                    <h5>€1,50</h5>
+                </div>
+                <div class="product-in-lower">
+                    <input type="number" id="quantity" name="quantity" min="1" max="10" placeholder="Aantal">
+                    <div class="btn-toevoegen">
+                        <a href="#">
+                            <h4>Toevoegen!</h4>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
 
         <div class="product item-9">
+            <div class="product-in"><img src="images/product11.png" alt="">
+                <div class="product-in-text">
+                    <h5>Slaatj</h5>
+                    <h5>€3,-</h5>
+                </div>
+                <div class="product-in-lower">
+                    <input type="number" id="quantity" name="quantity" min="1" max="10" placeholder="Aantal">
+                    <div class="btn-toevoegen">
+                        <a href="#">
+                            <h4>Toevoegen!</h4>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="product item-10">
+            <div class="product-in"><img src="images/product7.png" alt="">
+                <div class="product-in-text">
+                    <h5>Kippetj</h5>
+                    <h5>€3,-</h5>
+                </div>
+                <div class="product-in-lower">
+                    <input type="number" id="quantity" name="quantity" min="1" max="10" placeholder="Aantal">
+                    <div class="btn-toevoegen">
+                        <a href="#">
+                            <h4>Toevoegen!</h4>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="product item-11">
             <div class="product-in"><img src="images/product9.png" alt="">
-                <h5>broodj</h5>
+                <div class="product-in-text">
+                    <h5>Ijsj</h5>
+                    <h5>€2,50</h5>
+                </div>
+                <div class="product-in-lower">
+                    <input type="number" id="quantity" name="quantity" min="1" max="10" placeholder="Aantal">
+                    <div class="btn-toevoegen">
+                        <a href="#">
+                            <h4>Toevoegen!</h4>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="product item-12">
+            <div class="product-in"><img src="images/product10.png" alt="">
+                <div class="product-in-text">
+                    <h5>Drankj</h5>
+                    <h5>€1,50</h5>
+                </div>
+                <div class="product-in-lower">
+                    <input type="number" id="quantity" name="quantity" min="1" max="10" placeholder="Aantal">
+                    <div class="btn-toevoegen">
+                        <a href="#">
+                            <h4>Toevoegen!</h4>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
 
